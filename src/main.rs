1235aac3e74c14d3c4b40ee8fd1d9698e9182160
mishazawa <macintoshkrug@gmail.com>
2019-06-19 22:39:51 +0300

    midi

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 extern crate cpal;
+extern crate portmidi as pm;
 extern crate rand;
 
 use core::f32::consts::PI;
 use std::clone::{Clone};
+use std::time::Duration;
+use std::sync::mpsc;
+use std::thread;
+
 
 #[derive(Debug, Copy, Clone)]
 pub enum Waves {
@@ -86,7 +91,16 @@ impl Wavetable {
 
 fn amplify (v: f32, a: f32) -> f32 { v * a }
 
+fn midi_to_freq (key: u8) -> f32 {
+  (440.0 * (2.0f32).powf((key as f32 - 69.0) / 12.0)).floor()
+}
+
 fn main() {
+  let context = pm::PortMidi::new().unwrap();
+  let timeout = Duration::from_millis(10);
+  const BUF_LEN: usize = 1024;
+  let (tx, rx) = mpsc::channel();
+
   let device = cpal::default_output_device().expect("Failed to get default output device");
   let format = device.default_output_format().expect("Failed to get default output format");
 
@@ -97,21 +111,65 @@ fn main() {
 
   let sample_rate = format.sample_rate.0;
 
-  let mut sine = Wavetable::new(Waves::SINE, sample_rate as i32);
+  let mut n3 = Wavetable::new(Waves::SIN, sample_rate as i32);
 
-  let amp = 0.2;
+  thread::spawn(move || {
+    let in_ports = context
+      .devices()
+      .unwrap()
+      .into_iter()
+      .filter_map(|dev| context.input_port(dev, BUF_LEN).ok())
+      .collect::<Vec<_>>();
+
+    loop {
+      for port in &in_ports {
+        if let Ok(Some(events)) = port.read_n(BUF_LEN) {
+          tx.send((port.device(), events)).unwrap();
+        }
+      }
+      thread::sleep(timeout);
+    }
+  });
+
+  let mut last_freq = 0.0;
+  let mut is_note_playing = false;
 
   event_loop.run(move |_, data| {
     match data {
       cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::F32(mut buffer) } => {
-        for sample in buffer.chunks_mut(format.channels as usize) {
-          let v = sine.next_value(440.0);
-          for out in sample.iter_mut() {
-            *out = amplify(v, amp);
-          };
+        if is_note_playing {
+          for sample in buffer.chunks_mut(format.channels as usize) {
+            let v3 = n3.next_value(last_freq);
+            for out in sample.iter_mut() {
+              *out = amplify(v3, 0.5);
+            };
+          }
+        }
+        if !rx.try_recv().is_err() {
+          let (devices, events) = rx.recv().unwrap();
+          let midi_data1 = events.get(0).unwrap().message.data1;
+            println!("{:?}", events);
+          for event in events {
+            // match event.message.status {
+            //   0x80 => {
+            //     // depress
+            //     println!("depress");
+            //     is_note_playing = false;
+            //     last_freq = 0.0;
+            //   },
+            //   0x90 => {
+            //     // press
+            //     println!("press");
+            //     is_note_playing = true;
+            //     last_freq = midi_to_freq(midi_data1);
+            //   },
+            //   _ => ()
+            // }
+          }
         }
       },
       _ => (),
     }
   });
+
 }
