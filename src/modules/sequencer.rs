77ad8253597ffc8a981a3624e5160f4ab25fe5e5
mishazawa <macintoshkrug@gmail.com>
2019-07-01 12:49:32 +0300

    test seq

--- src/modules/sequencer.rs
+++ src/modules/sequencer.rs
@@ -1,32 +1,39 @@
 use std::collections::HashMap;
 use super::{ voice::Voice };
 
+
+const SEQ_LEN: usize = 16;
+
 #[derive(Debug)]
 struct Track {
   voice: Voice,
-  steps: [bool; 16]
-}
-
-#[derive(Debug)]
-pub struct Sequencer {
-  tempo: f32,
-  tracks: HashMap<String, Track>
+  steps: [bool; SEQ_LEN]
 }
 
 impl Track {
   fn new (voice: Voice) -> Self {
     Self {
       voice,
-      steps: [false; 16]
+      steps: [false; SEQ_LEN]
     }
   }
+}
+
+#[derive(Debug)]
+pub struct Sequencer {
+  tempo: f32,
+  tracks: HashMap<String, Track>,
+  pointer: u8,
+  playing: bool
 }
 
 impl Sequencer {
   pub fn new () -> Self {
     Self {
       tempo: 110.0,
-      tracks: HashMap::new()
+      tracks: HashMap::new(),
+      pointer: 0,
+      playing: false
     }
   }
 
@@ -59,4 +66,58 @@ impl Sequencer {
       }
     }
   }
+
+  pub fn play (&mut self, state: bool) {
+    self.playing = state;
+  }
+
+  pub fn next_step (&mut self) -> Vec<&String> {
+    self.pointer = (self.pointer + 1) % SEQ_LEN as u8;
+
+    let mut tracks_to_play = Vec::new();
+
+    for (key, track) in &self.tracks {
+      if track.steps[self.pointer as usize] {
+        tracks_to_play.push(key);
+      }
+    }
+    tracks_to_play
+  }
+
+}
+
+
+#[cfg(test)]
+
+#[test]
+fn it_works() {
+
+  use super::voice::{Voice};
+  let mut seq = Sequencer::new();
+  seq.tempo(100.5);
+
+  let voice = Voice {
+                note: 60,
+                freq: 440.0,
+                phase: 0.0,
+                sample_rate: 44100,
+                start_time: 0.0,
+                end_time: 0.0,
+                enabled: true
+              };
+
+  seq.add(String::from("sine"), voice);
+
+  for n in 0..SEQ_LEN {
+    if n % 2 == 0 {
+      seq.enable(String::from("sine"), n as u8);
+    } else {
+      seq.disable(String::from("sine"), n as u8);
+    }
+  }
+
+  seq.play(true);
+
+  assert_eq!(seq.next_step().len(), 0);
+  assert_eq!(seq.next_step().len(), 1);
 }
